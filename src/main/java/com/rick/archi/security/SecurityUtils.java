40a5c82bc2b6610198d3157960169ae68566d75f
eroszzh <zhenhzhu@126.com>
2016-02-21 21:08:10 +0800

    add some security method

--- src/main/java/com/rick/archi/security/SecurityUtils.java
+++ src/main/java/com/rick/archi/security/SecurityUtils.java
@@ -1,13 +1,24 @@
 package com.rick.archi.security;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class SecurityUtils {
 	
 	private static String bytesToHexString(byte[] src){   
-	    StringBuilder stringBuilder = new StringBuilder("");   
+	    StringBuilder stringBuilder = new StringBuilder();
 	    if (src == null || src.length <= 0) {   
 	        return null;   
 	    }   
@@ -41,6 +52,9 @@ public class SecurityUtils {
 	    return (byte) "0123456789ABCDEF".indexOf(c);   
 	}
 
+
+	//File type
+
 	private static String getFileHeader(String filePath) throws IOException {
 		byte[] b = new byte[28];
 		InputStream inputStream = null;
@@ -49,7 +63,13 @@ public class SecurityUtils {
 		inputStream.close();
 		return bytesToHexString(b);
 	}
-	
+
+	/**
+	 * check file type
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
 	public static FileType getType(String filePath) throws IOException {
 		String fileHead = getFileHeader(filePath);
 		if(fileHead == null || fileHead.length() == 0) {
@@ -64,6 +84,188 @@ public class SecurityUtils {
 		}
 		return null;
 	}
+
+	//hash
+
+	/**
+	 * md5 hash
+	 * @param content
+	 * @return
+	 * @throws Exception
+	 */
+	public static String md5(String content) throws Exception {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte[] bytes = md.digest(content.getBytes("utf8"));
+		return bytesToHexString(bytes);
+	}
+
+	/**
+	 * sha1
+	 * @param content
+	 * @return
+	 * @throws Exception
+	 */
+	public static String sha1(String content) throws Exception {
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		byte[] bytes = md.digest(content.getBytes("utf8"));
+		return bytesToHexString(bytes);
+	}
+
+	/**
+	 * sha512
+	 * @param content
+	 * @return
+	 * @throws Exception
+	 */
+	public static String sha512(String content) throws Exception {
+		MessageDigest md = MessageDigest.getInstance("SHA-512");
+		byte[] bytes = md.digest(content.getBytes("utf8"));
+		return bytesToHexString(bytes);
+	}
+
+	/**
+	 * base64 encode
+	 * @param bytes
+	 * @return
+	 */
+	public static String bytesToBase64(byte[] bytes) {
+		BASE64Encoder encoder = new BASE64Encoder();
+		return encoder.encode(bytes);
+	}
+
+	/**
+	 * base64 decode
+	 * @param base64
+	 * @return
+	 * @throws IOException
+	 */
+	public static byte[] base64ToBytes(String base64) throws IOException {
+		BASE64Decoder decoder = new BASE64Decoder();
+		return decoder.decodeBuffer(base64);
+	}
+
+
+	//DES
+
+	public static byte[] getKeyDES() throws NoSuchAlgorithmException {
+		KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+		keyGen.init(56);
+		SecretKey key = keyGen.generateKey();
+		return key.getEncoded();
+	}
+
+	public static SecretKey loadKeyDES(byte[] bytes) throws IOException {
+		SecretKey key = new SecretKeySpec(bytes, "DES");
+		return key;
+	}
+
+	public static String encryptDES(String source, SecretKey key) throws Exception {
+		Cipher cipher = Cipher.getInstance("DES");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] bytes = cipher.doFinal(source.getBytes());
+		return bytesToBase64(bytes);
+	}
+
+	public static String decryptDES(String source, SecretKey key) throws Exception{
+		Cipher cipher = Cipher.getInstance("DES");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] bytes = cipher.doFinal(base64ToBytes(source));
+		return new String(bytes, "utf8");
+	}
+
+	//AES
+	public static byte[] getKeyAES() throws NoSuchAlgorithmException {
+		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+		keyGen.init(128);
+		SecretKey key = keyGen.generateKey();
+		return key.getEncoded();
+	}
+
+	public static SecretKey loadKeyAES(byte[] bytes) throws IOException {
+		SecretKey key = new SecretKeySpec(bytes, "AES");
+		return key;
+	}
+
+	public static String encryptAES(String source, SecretKey key) throws Exception {
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] bytes = cipher.doFinal(source.getBytes());
+		return bytesToBase64(bytes);
+	}
+
+	public static String decryptAES(String source, SecretKey key) throws Exception{
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] bytes = cipher.doFinal(base64ToBytes(source));
+		return new String(bytes, "utf8");
+	}
+
+
+    //RSA
+
+    public static KeyPair getKeyPair() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(512);
+        KeyPair keyPair = keyGen.generateKeyPair();
+        return keyPair;
+    }
+
+    public static byte[] getPublicKey(KeyPair keyPair) {
+        PublicKey publicKey = keyPair.getPublic();
+        byte[] bytes = publicKey.getEncoded();
+        return bytes;
+    }
+
+    public static byte[] getPrivateKey(KeyPair keyPair) {
+        PrivateKey privateKey = keyPair.getPrivate();
+        byte[] bytes = privateKey.getEncoded();
+        return bytes;
+    }
+
+    public static PublicKey bytesToPublicKey(byte[] pubBytes) throws Exception {
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(pubBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        return publicKey;
+    }
+
+    public static PrivateKey bytesToPrivateKey(byte[] priBytes) throws Exception {
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(priBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    public static String publicEncrypt(String content, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] bytes = cipher.doFinal(content.getBytes());
+        return bytesToBase64(bytes);
+    }
+
+    public static String privateDecrypt(String content, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] bytes = cipher.doFinal(base64ToBytes(content));
+        return new String(bytes, "utf8");
+    }
+
+
+    //sign
+    public static String sign(String content, PrivateKey privateKey) throws Exception {
+        Signature signature = Signature.getInstance("SHA1withRSA"); //MD5withRSA, SHA1withRSA
+        signature.initSign(privateKey);
+        signature.update(content.getBytes());
+        return bytesToBase64(signature.sign());
+    }
+
+    public static boolean verify(String content, String sign, PublicKey publicKey) throws Exception {
+        Signature signature = Signature.getInstance("SHA1withRSA");
+        signature.initVerify(publicKey);
+        signature.update(content.getBytes());
+        return signature.verify(base64ToBytes(sign));
+    }
+
 	
 	
 }
--- src/test/java/com/rick/archi/security/TestSecurity.java
+++ src/test/java/com/rick/archi/security/TestSecurity.java
@@ -1,8 +1,11 @@
 package com.rick.archi.security;
 
 import java.io.IOException;
+import java.security.KeyPair;
 
 import org.junit.Test;
+
+import javax.crypto.SecretKey;
 
 public class TestSecurity {
 
@@ -10,5 +13,74 @@ public class TestSecurity {
 	public void testFileType() throws IOException {
 		System.out.println(SecurityUtils.getType("/home/eros/1.png").name());
 	}
+
+	@Test
+	public void testMD5() throws Exception {
+		System.out.println(SecurityUtils.md5("password"));
+	}
+
+    @Test
+    public void testSHA1() throws Exception {
+        System.out.println(SecurityUtils.sha1("password"));
+    }
+
+    @Test
+    public void testSHA512() throws Exception {
+        System.out.println(SecurityUtils.sha512("password"));
+    }
+
+    @Test
+    public void testBase64Encode() {
+        System.out.println(SecurityUtils.bytesToBase64("password".getBytes()));
+    }
+
+    @Test
+    public void testBase64Decode() throws IOException {
+        String s = new String(SecurityUtils.base64ToBytes("cGFzc3dvcmQ="),"utf8");
+        System.out.println(s);
+    }
+
+    @Test
+    public void testDES() throws Exception {
+        SecretKey key = SecurityUtils.loadKeyDES(SecurityUtils.getKeyDES());
+        String encrypted = SecurityUtils.encryptDES("password", key);
+        System.out.println(encrypted);
+        String decrypted = SecurityUtils.decryptDES(encrypted, key);
+        System.out.println(decrypted);
+    }
+
+    @Test
+    public void testAES() throws Exception {
+        SecretKey key = SecurityUtils.loadKeyAES(SecurityUtils.getKeyAES());
+        String encrypted = SecurityUtils.encryptAES("password", key);
+        System.out.println(encrypted);
+        String decrypted = SecurityUtils.decryptAES(encrypted, key);
+        System.out.println(decrypted);
+    }
+
+    @Test
+    public void testRSA() throws Exception {
+        String content = "password";
+        KeyPair keyPair = SecurityUtils.getKeyPair();
+        byte[] publicKey = SecurityUtils.getPublicKey(keyPair);
+        byte[] privateKey = SecurityUtils.getPrivateKey(keyPair);
+        System.out.println("public key: " + SecurityUtils.bytesToBase64(publicKey));
+        System.out.println("private key: " + SecurityUtils.bytesToBase64(privateKey));
+        String result = SecurityUtils.publicEncrypt(content, SecurityUtils.bytesToPublicKey(publicKey));
+        System.out.println("Encrypted: " + result);
+        String decrypted = SecurityUtils.privateDecrypt(result, SecurityUtils.bytesToPrivateKey(privateKey));
+        System.out.println("Decryped: " + decrypted);
+    }
+
+    @Test
+    public void testSign() throws Exception {
+        String content = "password";
+        KeyPair keyPair = SecurityUtils.getKeyPair();
+        byte[] publicKey = SecurityUtils.getPublicKey(keyPair);
+        byte[] privateKey = SecurityUtils.getPrivateKey(keyPair);
+        String sign = SecurityUtils.sign(content, SecurityUtils.bytesToPrivateKey(privateKey));
+        System.out.println("sign: " + sign);
+        System.out.println("result: " + SecurityUtils.verify(content, sign, SecurityUtils.bytesToPublicKey(publicKey)));
+    }
 
 }
